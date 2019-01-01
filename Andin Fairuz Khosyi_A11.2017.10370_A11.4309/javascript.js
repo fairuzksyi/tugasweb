f376fa03132fc37a797dfb25ecae75dfc1973f47
fairuzksyi <46293720+fairuzksyi@users.noreply.github.com>
2019-01-01 16:00:18 +0700

    Add files via upload

---
+++ Andin Fairuz Khosyi_A11.2017.10370_A11.4309/javascript.js
@@ -0,0 +1,29 @@
+	alert("Selamat Datang Di Website Saya,,, Enjoyyy :)");
+
+         var nama = prompt("Masukan Nama Sesuai KTP Anda:");
+   
+			function cek(){
+			    var msg = confirm("Apakah Anda yakin ingin berpindah halaman ?");
+			    if(msg==true){
+			    	window.location="";  
+			    			}
+			    else{
+			    	window.location="";
+			    			}
+			    }
+
+// function cek() {
+//                var retVal = confirm("Do you want to continue ?");
+//                if( retVal == true ) {
+//                   document.write ("User wants to continue!");
+//                   return true;
+//                } else {
+//                   document.write ("User does not want to continue!");
+//                   return false;
+//                }
+//             }
+
+       //      function AutoRefresh( t ) {
+       //         setTimeout("location.reload(true);", t);
+       //      }
+       // 
